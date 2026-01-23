fc9a88ad7e957ccf74b33fd419e6400014586f12
Aditya Venkatesh <yukiiiihayashi@gmail.com>
2026-01-23 12:00:00 +0530

    Day 14: Uploaded DSA solution for Day 14

---
+++ DAY14.c
@@ -0,0 +1,26 @@
+/*
+
+*/
+#include <stdio.h>
+
+int main() {
+    int m, n;
+    scanf("%d %d", &m, &n);
+
+    int matrix[m][n];
+    int sum = 0;
+
+    for(int i = 0; i < m; i++) {
+        for(int j = 0; j < n; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    for(int i = 0; i < m && i < n; i++) {
+        sum += matrix[i][i];
+    }
+
+    printf("%d", sum);
+
+    return 0;
+}
