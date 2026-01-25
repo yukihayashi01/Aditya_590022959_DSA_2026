6f76c7256e9836077a918e07a1599c2cd7647ad7
Aditya Venkatesh <yukiiiihayashi@gmail.com>
2026-01-25 12:00:00 +0530

    Day 16: Uploaded DSA solution for Day 16

---
+++ DAY16.c
@@ -0,0 +1,36 @@
+// Given an array of integers, count the frequency of each distinct element and print the result.
+
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int visited[n];
+    for(int i = 0; i < n; i++) {
+        visited[i] = 0;
+    }
+
+    for(int i = 0; i < n; i++) {
+        if(visited[i] == 1)
+            continue;
+
+        int count = 1;
+
+        for(int j = i + 1; j < n; j++) {
+            if(arr[i] == arr[j]) {
+                count++;
+                visited[j] = 1;
+            }
+        }
+
+        printf("%d:%d ", arr[i], count);
+    }
+
+    return 0;
+}
