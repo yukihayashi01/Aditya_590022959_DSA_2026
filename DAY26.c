a9f1bb6a0b08c012be603bb9d955ce8a3c39de1c
Aditya Venkatesh <yukiiiihayashi@gmail.com>
2026-02-04 12:00:00 +0530

    Day 26: Uploaded DSA solution for Day 26

---
+++ DAY26.c
@@ -0,0 +1,69 @@
+/*Problem: Doubly Linked List Insertion and Traversal - Implement using linked list with dynamic memory allocation.
+
+Input:
+- First line: integer n
+- Second line: n space-separated integers
+
+Output:
+- Print the linked list elements in forward order, space-separated
+
+Example:
+Input:
+5
+10 20 30 40 50
+
+Output:
+10 20 30 40 50
+
+Explanation:
+Each node has data, next, prev. Insert nodes sequentially, traverse from head using next pointer.*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+    struct Node* prev;
+};
+
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    newNode->prev = NULL;
+    return newNode;
+}
+
+void insertEnd(struct Node** head, int data) {
+    struct Node* newNode = createNode(data);
+    if (*head == NULL) {
+        *head = newNode;
+        return;
+    }
+    struct Node* temp = *head;
+    while (temp->next != NULL)
+        temp = temp->next;
+    temp->next = newNode;
+    newNode->prev = temp;
+}
+
+void traverseForward(struct Node* head) {
+    struct Node* temp = head;
+    while (temp != NULL) {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+}
+
+int main() {
+    int n, x;
+    struct Node* head = NULL;
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &x);
+        insertEnd(&head, x);
+    }
+    traverseForward(head);
+    return 0;
+}
