8ffdc860008f7db9341251aaefa63c8d478d9728
Aditya Venkatesh <yukiiiihayashi@gmail.com>
2026-03-08 12:00:00 +0530

    Day 58: Uploaded DSA solution for Day 58

---
+++ DAY58.c
@@ -0,0 +1,93 @@
+/*Problem Statement:
+Construct a binary tree from given preorder and inorder traversal arrays.
+
+Input Format:
+- First line contains integer N
+- Second line contains preorder traversal
+- Third line contains inorder traversal
+
+Output Format:
+- Print postorder traversal of constructed tree
+
+Example:
+Input:
+5
+1 2 4 5 3
+4 2 5 1 3
+
+Output:
+4 5 2 3 1
+
+Explanation:
+Preorder identifies root, inorder splits left and right subtrees.*/
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+int preIndex = 0;
+
+struct Node* createNode(int val) {
+    struct Node* node = (struct Node*)malloc(sizeof(struct Node));
+    node->data = val;
+    node->left = node->right = NULL;
+    return node;
+}
+
+int search(int inorder[], int start, int end, int val) {
+    for (int i = start; i <= end; i++) {
+        if (inorder[i] == val)
+            return i;
+    }
+    return -1;
+}
+
+struct Node* buildTree(int preorder[], int inorder[], int start, int end) {
+    if (start > end)
+        return NULL;
+
+    int curr = preorder[preIndex++];
+    struct Node* node = createNode(curr);
+
+    if (start == end)
+        return node;
+
+    int pos = search(inorder, start, end, curr);
+
+    node->left = buildTree(preorder, inorder, start, pos - 1);
+    node->right = buildTree(preorder, inorder, pos + 1, end);
+
+    return node;
+}
+
+void postorder(struct Node* root) {
+    if (root == NULL)
+        return;
+
+    postorder(root->left);
+    postorder(root->right);
+    printf("%d ", root->data);
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int preorder[n], inorder[n];
+
+    for (int i = 0; i < n; i++)
+        scanf("%d", &preorder[i]);
+
+    for (int i = 0; i < n; i++)
+        scanf("%d", &inorder[i]);
+
+    struct Node* root = buildTree(preorder, inorder, 0, n - 1);
+
+    postorder(root);
+
+    return 0;
+}
